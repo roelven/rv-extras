afb4ba071e9fa27623a0341c41b972186a855760
Roel van der Ven <roel@soundcloud.com>
2012-06-08 20:57:51 +0200

    do not disable widgets by default, add versioning functions

--- functions/functions.php
+++ functions/functions.php
@@ -309,4 +309,32 @@ function rv_resizeOembed($markup) {
   return preg_replace($patterns, $replacements, $markup);
 }
 
+// Custom Functions for CSS/Javascript Versioning
+$GLOBALS["TEMPLATE_URL"] = get_bloginfo('template_url')."/";
+$GLOBALS["TEMPLATE_RELATIVE_URL"] = wp_make_link_relative($GLOBALS["TEMPLATE_URL"]);
+
+// Add ?v=[last modified time] to style sheets
+function rv_versioned_stylesheet($relative_url, $add_attributes=""){
+  print '<link rel="stylesheet" href="http://' .$_SERVER['HTTP_HOST'].$GLOBALS["TEMPLATE_RELATIVE_URL"].rv_versioned_resource($relative_url).'" '.$add_attributes.'>'."\n";
+}
+
+// Add ?v=[last modified time] to javascripts
+function rv_versioned_javascript($relative_url, $add_attributes=""){
+  print '<script src="http://' .$_SERVER['HTTP_HOST'].$GLOBALS["TEMPLATE_RELATIVE_URL"].rv_versioned_resource($relative_url).'" '.$add_attributes.'></script>'."\n";
+}
+
+// Add ?v=[last modified time] to a file url
+function rv_versioned_resource($relative_url){
+  // todo: make theme name variable
+  $file = $_SERVER["DOCUMENT_ROOT"]."/wp-content/themes/blomblom/".$relative_url;
+  $file_version = "";
+
+  if(file_exists($file)) {
+    $file_version = "?v=".filemtime($file);
+  }
+
+  return $relative_url.$file_version;
+}
+
+
 ?>
